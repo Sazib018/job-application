e95783b6f7b1fec41b4c8f2ab9e49087c52f97a3
Sazib018 <mahiburislamsazib@gmail.com>
2025-03-24 01:48:59 -0500

    add

--- src/Components/Home/AnimatedImages/AnimatedImages.jsx
+++ src/Components/Home/AnimatedImages/AnimatedImages.jsx
@@ -2,23 +2,22 @@ import { motion } from "framer-motion";
 
 const AnimatedImages = () => {
   return (
-    <div className="flex justify-center items-center h-screen relative overflow-hidden">
+    <div className="flex justify-center items-center h-screen relative overflow-hidden ">
       <motion.img
         src="https://i.ibb.co/2QhkJTh/images.jpg"
-        alt=""
-        className="absolute w-2/5 h-2/4 rounded-r-2xl rounded-bl-2xl z-10"
+        alt="First Image"
+        className="absolute w-full max-w-sm md:max-w-md lg:max-w-lg rounded-r-2xl rounded-bl-2xl "
         initial={{ y: -200, opacity: 0 }}
         animate={{ y: -100, opacity: 1 }}
-        transition={{ duration: 1, repeat: Infinity  }}
-      />
+        transition={{ duration: 1, repeat: Infinity}}/>
+
       <motion.img
         src="https://i.ibb.co.com/XxHYrG0s/images-1.jpg"
-        alt=""
-        className="absolute w-2/5 h-2/4 rounded-r-2xl rounded-bl-2xl z-0"
+        alt="Second Image"
+        className="absolute w-full max-w-sm md:max-w-md lg:max-w-lg rounded-r-2xl rounded-bl-2xl"
         initial={{ x: 200, opacity: 0 }}
         animate={{ x: 100, opacity: 1 }}
-        transition={{ duration: 1, repeat: Infinity  }}
-      />
+        transition={{ duration: 1, repeat: Infinity}}/>
     </div>
   );
 };
